4c4765a105dcf682f68f029c9183e11e7a3eca85
Barak-S <barak.saidoff@gmail.com>
2021-01-06 17:18:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
         <Switch>
           <Route exact path= "/" render={props=>(<Home {...props}/>)}/>
+          {/* <Route exact path= "/" render={props=>(<Home {...props}/>)}/> */}
         </Switch>   
       </div>
     </BrowserRouter>
