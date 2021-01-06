1ceb7637617a6605ed5d6204c63100f5598f8c89
Barak-S <barak.saidoff@gmail.com>
2021-01-06 17:02:02 -0500

    first commit

---
+++ src/containers/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p>home</p>
+            <div className="banner-div">
+                <div class="diagonal-top"></div>
+                <div class="diagonal-middle">
+                    <div className="row">
+                        <div className="col">
+                            <p>The Crypto Hub</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="diagonal-bottom"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
